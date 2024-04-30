1c291e9dd59590d9fa0e15428d2ba70f50791c9a
marianayee4 <167908943+marianayee4@users.noreply.github.com>
2024-04-30 16:37:45 -0300

    o que fiz hoje
    criei metodo para escrever o arquivo

--- lista de contatos/Form1.Designer.cs
+++ lista de contatos/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.Mariana = new System.Windows.Forms.TextBox();
+            this.textBoxnome = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxsobrenome = new System.Windows.Forms.TextBox();
+            this.textBoxtelefone = new System.Windows.Forms.TextBox();
+            this.buttonincluircontatos = new System.Windows.Forms.Button();
             this.listBoxcontatos = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
@@ -47,12 +47,12 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome";
             // 
-            // Mariana
+            // textBoxnome
             // 
-            this.Mariana.Location = new System.Drawing.Point(152, 86);
-            this.Mariana.Name = "Mariana";
-            this.Mariana.Size = new System.Drawing.Size(100, 20);
-            this.Mariana.TabIndex = 1;
+            this.textBoxnome.Location = new System.Drawing.Point(152, 86);
+            this.textBoxnome.Name = "textBoxnome";
+            this.textBoxnome.Size = new System.Drawing.Size(100, 20);
+            this.textBoxnome.TabIndex = 1;
             // 
             // label2
             // 
@@ -72,33 +72,33 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Telefone";
             // 
-            // textBox2
+            // textBoxsobrenome
             // 
-            this.textBox2.Location = new System.Drawing.Point(152, 133);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
+            this.textBoxsobrenome.Location = new System.Drawing.Point(152, 133);
+            this.textBoxsobrenome.Name = "textBoxsobrenome";
+            this.textBoxsobrenome.Size = new System.Drawing.Size(100, 20);
+            this.textBoxsobrenome.TabIndex = 4;
             // 
-            // textBox3
+            // textBoxtelefone
             // 
-            this.textBox3.Location = new System.Drawing.Point(152, 182);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 5;
+            this.textBoxtelefone.Location = new System.Drawing.Point(152, 182);
+            this.textBoxtelefone.Name = "textBoxtelefone";
+            this.textBoxtelefone.Size = new System.Drawing.Size(100, 20);
+            this.textBoxtelefone.TabIndex = 5;
             // 
-            // button1
+            // buttonincluircontatos
             // 
-            this.button1.Location = new System.Drawing.Point(62, 276);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 120);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonincluircontatos.Location = new System.Drawing.Point(62, 276);
+            this.buttonincluircontatos.Name = "buttonincluircontatos";
+            this.buttonincluircontatos.Size = new System.Drawing.Size(190, 120);
+            this.buttonincluircontatos.TabIndex = 6;
+            this.buttonincluircontatos.Text = "button1";
+            this.buttonincluircontatos.UseVisualStyleBackColor = true;
             // 
             // listBoxcontatos
             // 
             this.listBoxcontatos.FormattingEnabled = true;
-            this.listBoxcontatos.Location = new System.Drawing.Point(370, 86);
+            this.listBoxcontatos.Location = new System.Drawing.Point(373, 86);
             this.listBoxcontatos.Name = "listBoxcontatos";
             this.listBoxcontatos.Size = new System.Drawing.Size(284, 303);
             this.listBoxcontatos.TabIndex = 7;
@@ -109,12 +109,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.listBoxcontatos);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonincluircontatos);
+            this.Controls.Add(this.textBoxtelefone);
+            this.Controls.Add(this.textBoxsobrenome);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.Mariana);
+            this.Controls.Add(this.textBoxnome);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -127,12 +127,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox Mariana;
+        private System.Windows.Forms.TextBox textBoxnome;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxsobrenome;
+        private System.Windows.Forms.TextBox textBoxtelefone;
+        private System.Windows.Forms.Button buttonincluircontatos;
         private System.Windows.Forms.ListBox listBoxcontatos;
     }
 }
--- lista de contatos/Form1.cs
+++ lista de contatos/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace lista_de_contatos
 {
@@ -16,7 +18,30 @@ namespace lista_de_contatos
         {
             InitializeComponent();
         }
+        private Contato[] listadeContatos = new Contato[1];
 
+        private void Escrever(Contato contato)
+        {
+            StreamWriter escrever = new SreamWritterr("Contatos.txt");
+
+            listaDeContatos = new Contato[Convert.ToInt32(Ler.ReadLine()];
+ 
+
+          escrever.WriteLine(ListaDeContatos.Length + 1);
+            escrever.WriteLine(contato.Nome);
+            escrever.WriteLine(contato.Sobrenome);
+            escrever.WriteLine(contato.Telefone);
+
+            for (int x = 0) ; x < listaDeContatos.Lenght; x++)
+          {
+                escrever.WriteLine(listaDeContatos[x].Nome);
+                escrever.WriteLine(listaDeContatos[
+                escrever.W
+
+
+                escrever.
+              }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
